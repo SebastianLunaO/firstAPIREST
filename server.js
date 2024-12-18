2b04205eee4d6c6d401e98259f7602a025a74e05
Sebastian Luna <newmsloh@gmail.com>
2024-12-17 21:43:31 -0600

    creating a simple api for functionality

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log('first')
+const PORT= process.env.PORT 
+const http = require('http')
+const products = require('./data/data.json')
+
+const server = http.createServer((req,res)=> {
+    if (req.url === '/api/products' && req.method==='GET'){
+        res.writeHead(200,{'content-type':'application/json'});
+        res.write(JSON.stringify(products));
+        res.end()
+    }else{
+        res.writeHead(404,{'content-type':'application/json'});
+        res.write(JSON.stringify('Request not Found'));
+        res.end();
+    }
+
+   
+})
+
+
+server.listen(PORT,()=>{
+    console.log(`Starting server at ${PORT}`);
+})
